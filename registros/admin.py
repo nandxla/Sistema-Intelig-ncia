2da045ff9cae0556e0524e1fe8a97132a4d1946e
Fernando Santos <nandosantos.ia@gmail.com>
2024-03-01 11:03:50 -0300

    add models in admin

--- registros/admin.py
+++ registros/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class FornecedorAdmin(admin.ModelAdmin):
+    ...
+    
+admin.site.register(
+    models.Fornecedor,  # registrando model
+    FornecedorAdmin,    # registrando modelAdmin 
+)
--- registros/models.py
+++ registros/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Fornecedor(models.Model):
+    titular_choices = {
+        "PF": "Conta PF",
+        "PJ": "Conta PJ",
+    }
+    conta_choices = {
+        "COR": "Corrente",
+        "POU": "Poupança",
+    }
+    
+    cnpj_cpf = models.CharField(max_length=20)
+    razao_social = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    tipo_titular = models.CharField(max_length=100)
+    tipo_titular = models.CharField(
+        choices=titular_choices, 
+        max_length=10,
+        default="PJ",
+    )
+    tipo_conta = models.CharField(
+        choices=conta_choices,
+        max_length=10,
+        default="COR"
+    )
+    
+    def __str__(self) -> str:
+        return self.cnpj_cpf
